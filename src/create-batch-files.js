f5de98c63cdaed31cef5b9417e09b24356424643
Julien Etienne <j8@envidia.io>
2023-04-19 04:11:57 +0100

    Added readBatchConfig

--- src/create-batch-files.js
+++ src/create-batch-files.js
@@ -1,22 +1,36 @@
 import path from 'path'
-import { readdir } from 'fs/promises'
+import { readdir, readFile } from 'fs/promises'
 const { resolve } = path
 // const rootPath = new URL(path.dirname(import.meta.url)).pathname;
 
+const readBatchConfig = async (batchConfigPath) => {
+
+    const file = readFile(batchConfigPath, 'utf8', (err, data) => {
+        console.log('read batch config :::', batchConfigPath)
+        if (err) {
+            console.error(err)
+            return
+        }
+        return Buffer.from(data)
+    })
+
+    console.log(await file)
+}
 
 /**
  * Find all batch.json files within the parent and sub directories.
- * @param {string} parentDirectory 
+ * @param {string} parentDirectory
  */
 const findBatchConfigFiles = async parentDirectory => {
     async function* findFiles(dir) {
-        const dirents = await readdir(dir, { withFileTypes: true });
+        const dirents = await readdir(dir, { withFileTypes: true })
         for (const dirent of dirents) {
-            const res = resolve(dir, dirent.name);
+            const res = resolve(dir, dirent.name)
             if (dirent.isDirectory()) {
                 yield* findFiles(res)
             } else {
                 if (res.endsWith('batch.json')) {
+                    readBatchConfig(res)
                     yield res
                 }
             }
@@ -24,7 +38,7 @@ const findBatchConfigFiles = async parentDirectory => {
     }
 
     for await (const file of findFiles(parentDirectory)) {
-        console.log(file);
+        // console.log(file)
     }
 }
 

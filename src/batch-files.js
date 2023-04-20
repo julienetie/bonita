118d831b1774f4f23cb93a9a6a83ff60a2118006
Julien Etienne <j8@envidia.io>
2023-04-20 03:18:34 +0100

    Cleaned up

--- src/batch-files.js
+++ src/batch-files.js
@@ -2,30 +2,8 @@ import { rollup } from 'rollup'
 import path from 'path'
 import multi from '@rollup/plugin-multi-entry'
 import terser from '@rollup/plugin-terser'
-// import ignore from 'rollup-plugin-ignore'
-import nodeResolve from '@rollup/plugin-node-resolve'
 
-console.log('builtinModules'.builtinModules)
 const { resolve } = path
-
-function resolvePath(file, origin) {
-  // Your way to resolve local include path
-  console.log(file, origin)
-}
-
-function pathResolve(options) {
-  return {
-      resolveId: function(file, origin) {
-        console.log('file', file, origin)
-          // Your local include path must either starts with `./` or `../`
-          if (file.startsWith('./') || file.startsWith('../')) {
-              // Return an absolute include path
-              return resolvePath(file, origin);
-          }
-          return null; // Continue to the next plugins!
-      }
-  };
-}
 
 const batchFiles = async (
   batchList,
@@ -33,7 +11,6 @@ const batchFiles = async (
   dir,
   minify
 ) => {
-
   const file = resolve(dir, output)
   let bundle
   let buildFailed = false
@@ -46,19 +23,11 @@ const batchFiles = async (
         if (/external dependency/.test(message)) return
         console.error(message)
       },
-      external: (a,b,c) => {
-        // console.log(b,c)
-        return !input.includes(a)
-      },
+      external: (a, b, c) => !input.includes(a),
       input,
       plugins: [
         multi(),
-        minify && terser(),
-        // ignore(['*']),
-        // nodeResolve({
-        //   modulesOnly: true
-        // }),
-        pathResolve()
+        minify && terser()
       ]
     })
     await bundle.write({
